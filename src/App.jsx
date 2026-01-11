458de985bb8bfb0de275e8d22c27e0e705797e94
ayano <ayano.hokulea@gmail.com>
2026-01-11 13:14:33 +1100

    Add basic footer

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import { Routes, Route } from 'react-router-dom';
 import Home from './pages/Home';
 import Navbar from './components/Navbar';
+import Footer from './components/Footer';
 
 function App() {
   return (
@@ -10,6 +11,8 @@ function App() {
       <Routes>
         <Route path="/" element={<Home />} />
       </Routes>
+
+      <Footer />
     </>
   )
 }
---
+++ src/components/Footer.jsx
@@ -0,0 +1,11 @@
+import './Footer.css';
+
+function Footer() {
+    return (
+        <footer>
+            <p>&copy; 2026 my little adventures. All rights reserved.</p>
+        </footer>
+    )
+}
+
+export default Footer
