0ae5b56b74566f61dfffc72760cb0b7ca2375ee0
ayano <ayano.hokulea@gmail.com>
2026-01-13 14:35:19 +1100

    Add extra filter for region

--- src/pages/Blog.jsx
+++ src/pages/Blog.jsx
@@ -5,30 +5,34 @@ import { useState } from 'react';
 
 // mock data
 const trips = [
-    { id: 1, city: "Adelaide - Darwin", title: "The Ghan", year: 2025, lat: -34.9285, lng: 138.6007,  },
-    { id: 2, city: "Dimboola", title: "Pink Lake", year: 2025, lat: -36.4555, lng: 142.0275 },
-    { id: 3, city: "Tokyo", title: "Home", year: 2026, lat: 35.68, lng: 139.77 },
-    { id: 4, city: "Den Haag", title: "The Netherlands", year: 2022, lat: 52.0705, lng: 4.3007 }
+    { id: 1, city: "Adelaide - Darwin", title: "The Ghan", year: 2025, region: "Oceania", lat: -34.9285, lng: 138.6007,  },
+    { id: 2, city: "Dimboola", title: "Pink Lake", year: 2025, region: "Oceania", lat: -36.4555, lng: 142.0275 },
+    { id: 3, city: "Tokyo", title: "Home", year: 2026, region: "Asia", lat: 35.68, lng: 139.77 },
+    { id: 4, city: "Den Haag", title: "The Netherlands", region: "Europe", year: 2022, lat: 52.0705, lng: 4.3007 }
 ];
 
 const tripYears = [...new Set(trips.map((t) => t.year))].sort((a, b) => a - b);
+const tripRegions = ["Asia", "Oceania", "Europe", "Africa", "North America"];
 
 
 function Blog() {
 
     // filter markers
     const [yearFilter, setYearFilter] = useState([]);
-    const filteredTrips = trips.filter(
-        trip => yearFilter.length == 0 || yearFilter.includes(trip.year)
-    );
+    const [regionFilter, setRegionFilter] = useState([]);
+    const filteredTrips = trips.filter( trip => {
+        const yearMatch = yearFilter.length == 0 || yearFilter.includes(trip.year);
+        const regionMatch = regionFilter.length == 0 || regionFilter.includes(trip.region);
+        return yearMatch && regionMatch;
+    });
 
     return (
         <div className="adventures">
             <header>my adventures around the world</header>
 
             <main>
-                <div className="filter">
-                    <div className="yearFilter">
+                <div className="filters">
+                    <div className="filter">
                         <h4>Year</h4>
                         {tripYears.map((year) => (
                             <div className="filterOption" key={year}>
@@ -39,6 +43,22 @@ function Blog() {
                                         setYearFilter(yearFilter.filter(y => y !== value));
                                     } else {
                                         setYearFilter([...yearFilter, value]);
+                                    }
+                                }} />
+                            </div>
+                        ))}
+                    </div>
+                    <div className="filter">
+                        <h4>Region</h4>
+                        {tripRegions.map((region) => (
+                            <div className="filterOption" key={region}>
+                                <label htmlFor={region}>{region}</label>
+                                <input type="checkbox" name={region} id={region} value={region} onChange={(e) => {
+                                    const value = e.target.value;
+                                    if (regionFilter.includes(region)) {
+                                        setRegionFilter(regionFilter.filter(r => r !== value));
+                                    } else {
+                                        setRegionFilter([...regionFilter, value]);
                                     }
                                 }} />
                             </div>
