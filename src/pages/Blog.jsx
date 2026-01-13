96961e7a44893abc67bf6d5a4236749605adf95b
ayano <ayano.hokulea@gmail.com>
2026-01-13 12:35:41 +1100

    Add basic style to markers

--- src/pages/Blog.jsx
+++ src/pages/Blog.jsx
@@ -2,19 +2,18 @@ import './Blog.css';
 import { MapContainer, TileLayer, Marker, Popup } from 'react-leaflet';
 import "leaflet/dist/leaflet.css";
 
+// mock data
 const trips = [
-    { id: 1, city: "Adelaide - Darwin", title: "The Ghan", lat: -34.9285, lng: 138.6007,  },
-    { id: 2, city: "Dimboola", title: "Pink Lake", lat: -36.4555, lng: 142.0275 }
+    { id: 1, city: "Adelaide - Darwin", title: "The Ghan", year: "2025", lat: -34.9285, lng: 138.6007,  },
+    { id: 2, city: "Dimboola", title: "Pink Lake", year: "2025", lat: -36.4555, lng: 142.0275 },
+    { id: 3, city: "Tokyo", title: "Home", year: "2026", lat: 35.68, lng: 139.77 }
 ];
-
 
 
 function Blog() {
     return (
         <div className="adventures">
             <header>my adventures around the world</header>
-
-
 
             <main>
                 <div className='map-wrapper'>
@@ -30,9 +29,13 @@ function Blog() {
                     {/* markers */}
                     {trips.map(trip => (
                         <Marker key = {trip.id} position={[trip.lat, trip.lng]}>
-                            <Popup className='markerPopup'>
-                                <h3>{trip.title}</h3>
-                                <p style={{ margin: "0px" }}>{trip.city}</p>
+                            <Popup>
+                                <div className="markerPopup">
+                                    <a href={`/adventure/${trip.id}`}>
+                                        <h3>{trip.title}</h3>
+                                    </a>
+                                        <p>{trip.city}</p>
+                                </div>
                             </Popup>
                         </Marker>
                     ))}
