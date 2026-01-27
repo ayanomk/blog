0f5609a7ff0d0c9523ddacbe1d30160c94701efa
ayano <ayano.hokulea@gmail.com>
2026-01-27 17:19:51 +1030

    Create hamburger lines and animation

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -2,17 +2,27 @@ import { NavLink } from "react-router-dom";
 
 import "./Navbar.css"
 import Logo from "./Logo"
+import { useState } from "react";
 
 function Navbar() {
+    // HAMBURGER
+    const [hamburgerOpen, setHamburgerOpen] = useState(false);
+
     return (
         <nav>
             <NavLink to="/" className="logo">
                 <Logo />
             </NavLink>
+
             <div className="nav-items">
                 <NavLink to="/adventures" className={({ isActive }) => isActive ? "active" : ""}>Adventures</NavLink>
                 <NavLink to="/aboutme" className={({ isActive }) => isActive ? "active" : ""}>About me</NavLink>
             </div>
+
+            {/* hamburger */}
+            <button className={`hamburger ${hamburgerOpen ? "open" : ""}`} onClick={() => setHamburgerOpen(!hamburgerOpen)}>
+                <span />
+            </button>
         </nav>
     )
 }
