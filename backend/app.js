8bf4ff71ca7bc2ad6e526faf544c76f429a65f54
ayano <ayano.hokulea@gmail.com>
2026-01-30 12:26:51 +1030

    Create and use asyncWrapper to remove try catch

--- backend/app.js
+++ backend/app.js
@@ -2,7 +2,8 @@ const express = require("express");
 const cors = require("cors");
 const posts = require("./data/mockData.json");
 
-const { errorHandler } = require("./middleware/errorHandler");
+const { errorHandler } = require("./middleware/errorHandler.js");
+const { asyncWrapper } = require("./utils/asyncWrapper.js");
 
 const app = express();
 
@@ -10,42 +11,34 @@ app.use(cors());
 app.use(express.json());
 
 // GET all posts
-app.get("/api/blogs", async (req, res, next) => {
-    try {
-        // FIXME! switch after connecting to db
-        // const data = await posts.findAll();
-        const data = posts;
-        // fail
-        if (!data) {
-            const err = new Error("No blogs found");
-            err.status = 404;
-            throw err;
-        }
-        //success
-        success(res, `All blog fetched successfully`, data);
-    } catch (err) {
-        next(err);
+app.get("/api/blogs", asyncWrapper(async (req, res) => {
+    // FIXME! switch after connecting to db
+    // const data = await posts.findAll();
+    const data = posts;
+    // fail
+    if (!data || data.length === 0) {
+        const err = new Error("No blogs found");
+        err.status = 404;
+        throw err;
     }
-});
+    // success
+    success(res, `All blog fetched successfully`, data);
+}));
 
 // GET single post by id
-app.get("/api/blogs/:id", async (req, res, next) => {
-    try {
-        // FIXME! switch after connecting to db
-        // const data = await posts.findById(req.params.id);
-        const data = posts.find(p => p.id == req.params.id);
-        // fail
-        if (!data) {
-            const err = new Error(`Blog ID: ${req.params.id} not found`);
-            err.status = 404;
-            throw err;
-        }
-        //success
-        success(res, `Blog ID: ${req.params.id} fetched successfully`, data);
-    } catch (err) {
-        next(err);
+app.get("/api/blogs/:id", asyncWrapper(async (req, res) => {
+    // FIXME! switch after connecting to db
+    // const data = await posts.findById(req.params.id);
+    const data = posts.find(p => p.id == req.params.id);
+    // fail
+    if (!data) {
+        const err = new Error(`Blog ID: ${req.params.id} not found`);
+        err.status = 404;
+        throw err;
     }
-})
+    //success
+    success(res, `Blog ID: ${req.params.id} fetched successfully`, data);
+}))
 
 // ERROR
 app.use(errorHandler);
