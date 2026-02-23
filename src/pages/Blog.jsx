5c82e10a41f5d4b5d4ada7cc9c16b68ed5304cc6
ayano <ayano.hokulea@gmail.com>
2026-02-23 19:46:50 +1030

    Simplify data structure and update frontend to match it

--- src/pages/Blog.jsx
+++ src/pages/Blog.jsx
@@ -7,6 +7,16 @@ import { getBlogById, getBlogsByFilter } from "../services/blogService.js";
 // Create HTML
 const htmlRenderer = (block, blockIdx) => {
     switch (block.type) {
+        case "header1":
+            return (
+                <h2 key={blockIdx}>{block.content}</h2>
+            )
+        case "header2":
+            return (
+                <h3 key={blockIdx}>{block.content}</h3>
+            )
+        case "text":
+            return <p key={blockIdx}>{block.content}</p>
         case "table":
             return (
                 <table key={blockIdx}>
@@ -28,10 +38,14 @@ const htmlRenderer = (block, blockIdx) => {
                     </tbody>
                 </table>
             )
-        case "text":
-            return <p key={blockIdx}>{block.content}</p>
         case "img":
-            return <img key={blockIdx} className={block.dir} src={block.src} alt="" loading="lazy" />
+            return (
+                <div className="imgBlock">
+                    {block.src.map((el, elIdx) => (
+                        <img key={elIdx} className={block.dir} src={el} alt="" loading="lazy" />
+                    ))}
+                </div>
+            )
         default:
             return null
     }
@@ -95,15 +109,12 @@ function Blog() {
                     if (section.sectionType == "info") {
                         return (
                         <aside className={section.sectionType} key={idx}>
-                            {section.subsections && section.subsections.map((sub, subIdx) => {
+                            {section.blocks && section.blocks.map((block, blockIdx) => {
                                 return (
-                                    <div className='asideSub' key={subIdx}>
-                                        <h2>{sub.title}</h2>
-                                        {sub.blocks.map((block, blockIdx) => {
-                                            return htmlRenderer(block, blockIdx);
-                                        })}
+                                    <div className='mainBlock' key={blockIdx}>
+                                        {htmlRenderer(block, blockIdx)}
                                     </div>
-                                )
+                                    )
                             })}
                         </aside>
                         )
@@ -112,15 +123,11 @@ function Blog() {
                     } else if (section.sectionType == "content") {
                         return (
                             <section className={section.sectionType} key={idx}>
-                                <h2>{section.section}</h2> 
-                                {section.subsections && section.subsections.map((sub, subIdx) => (
-                                    <div className='subsection' key={subIdx}>
-                                        <h3>{sub.title}</h3>
-                                        {sub.blocks.map((block, blockIdx) => {
-                                            return htmlRenderer(block, blockIdx);
-                                        })}
+                                {section.blocks && section.blocks.map((block, blockIdx) => (
+                                    <div className='mainBlock' key={blockIdx}>
+                                        {htmlRenderer(block, blockIdx)}
                                     </div>
-                                ))}
+                                    ))}
                             </section>
                         )
                     }
