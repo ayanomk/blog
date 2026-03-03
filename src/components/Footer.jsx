b1944df64963ded255c123723979b512450a0561
ayano <ayano.hokulea@gmail.com>
2026-03-03 14:22:25 +1030

    Create login button

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -4,6 +4,9 @@ function Footer() {
     return (
         <footer>
             <p>&copy; 2026 my little adventures. All rights reserved.</p>
+            <div>
+                <button>Login</button>
+            </div>
         </footer>
     )
 }
