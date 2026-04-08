5c9274b0f9136d7afd14bff81ddfa72ccabfdd2e
ayano <ayano.hokulea@gmail.com>
2026-04-08 17:33:02 +0930

    Clean up display of backend error message in terminal in production

--- backend/middleware/errorHandler.js
+++ backend/middleware/errorHandler.js
@@ -1,11 +1,16 @@
 const { failResponse } = require("../utils/response.js");
 
 const errorHandler = (err, req, res, next) => {
-    console.error(err);
-    
+    console.error({
+        message: err.message,
+        ...(process.env.NODE_ENV === "development" && {stack: err.stack}),
+    });    
+
     const code = err.statusCode || 500;
+
+    const message = process.env.NODE_ENV === "development" ? err.message : code >= 500 ? "Internal server error" : err.message;
     
-    failResponse(res, code, err.message || "Internal server error", err.data);
+    failResponse(res, code, message);
 };
 
 module.exports = { errorHandler };
