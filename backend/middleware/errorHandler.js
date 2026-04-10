5adae0b4a3a935ce44aa7e88aafe76495851fd30
ayano <ayano.hokulea@gmail.com>
2026-04-10 15:26:38 +0930

    Add error handling for mongodb validation fails

--- backend/middleware/errorHandler.js
+++ backend/middleware/errorHandler.js
@@ -4,7 +4,15 @@ const errorHandler = (err, req, res, next) => {
     console.error({
         message: err.message,
         ...(process.env.NODE_ENV === "development" && {stack: err.stack}),
-    });    
+    });
+
+    if (err.name === "ValidationError") {
+        const msg = Object.values(err.errors).map(val => val.message);
+        return failResponse(res, 400, msg);
+    }
+    if (err.name === "CastError") {
+        return failResponse(res, 400, "Invalid ID");
+    }
 
     const code = err.statusCode || 500;
 
